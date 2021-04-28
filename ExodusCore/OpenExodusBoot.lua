ef987c8613eedbd99c7dd4840a61eb2693f106a9
SeemdmAx <seemdmax@gmail.com>
2021-04-28 10:53:36 +0200

    boot

--- ExodusCore/OpenExodusBoot.lua
+++ ExodusCore/OpenExodusBoot.lua
@@ -3,7 +3,7 @@ local filesystem = require("filesystem")
 
 function OpenExodusBoot.preInitialization()
   ------ Overrites the package.path so the OpenExodusPackages are loadable ------
-  package.path = "/OpenExodus/?/?.lua;/OpenExodus/?.lua;/lib/?.lua;/usr/lib/?.lua;/home/lib/?.lua;./?.lua;/lib/?/init.lua;/usr/lib/?/init.lua;/home/lib/?/init.lua;./?/init.lua"
+  package.path = "/OpenExodus/ExodusCore/?.lua;/OpenExodus/?/?.lua;/OpenExodus/?.lua;/lib/?.lua;/usr/lib/?.lua;/home/lib/?.lua;./?.lua;/lib/?/init.lua;/usr/lib/?/init.lua;/home/lib/?/init.lua;./?/init.lua"
   if filesystem.exists("/OpenExodus/ExodusCore/OpenExodusGui.lua") and filesystem.exists("/OpenExodus/ExodusCore/OpenExodusProperties.lua") and filesystem.exists("/OpenExodus/ExodusCore/OpenExodusGitDownloader.lua") then
     return true
   else
