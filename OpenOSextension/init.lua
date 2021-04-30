0743edda68cb304ee61feb1c054c082b568ec17e
SeemdmAx <seemdmax@gmail.com>
2021-04-30 22:08:17 +0200

    Boot fail update

--- OpenOSextension/init.lua
+++ OpenOSextension/init.lua
@@ -14,14 +14,43 @@ do
   loadfile("/OpenExodus/ExodusCore/OpenExodusBoot.lua")(loadfile)
 end
 
-while true do
-  local result, reason = xpcall(require("shell").getShell(), function(msg)
-    return tostring(msg).."\n"..debug.traceback()
-  end)
-  if not result then
-    io.stderr:write((reason ~= nil and tostring(reason) or "unknown error") .. "\n")
-    io.write("Press any key to continue.\n")
-    os.sleep(0.5)
-    require("event").pull("key")
+local filesystem = require("filesystem")
+local string = require("string")
+local shell = require("shell")
+
+if filesystem.exists("/OpenExodus/ExodusCore/OpenExodusBoot.lua") == false then
+  if filesystem.exists("/OpenExodus/ExodusCore") == false then
+    print("Critical error, your systemfiles are not available!")
+    while true do
+      io.write("Reinstall system (Y/N): ")
+      local reinstall = io.read()
+      if string.upper(reinstall) == "Y" then
+        shell.execute("pastebin run g6xm54cJ")
+      elseif string.upper(reinstall) == "N" then
+        print("System shutdown, we can't do something for you, please contact the developer")
+        shell.execute("shutdown")
+      else
+        print("Invaild input!")
+      end
+    end
+  else
+    print("OpenExodusBoot-File not found!")
+    print("Downloading...")
+    shell.execute('wget -fq "https://raw.githubusercontent.com/SeemdmAx/OpenExodus/master/ExodusCore/OpenExodusBoot.lua" "/OpenExodus/ExodusCore/OpenExodusBoot.lua"')
+    print("Finished download")
+    shell.execute("reboot")
   end
-end
+else
+  print("System fail!")
+  while true do
+    io.write("Reboot or shutdown (R/S): ")
+    local action = io.read()
+    if action == string.upper("R") then
+      shell.execute("reboot")
+    elseif action == string.upper("S") then
+      shell.execute("shutdown")
+    else
+      print("Invaild input!")
+    end
+  end
+end 
