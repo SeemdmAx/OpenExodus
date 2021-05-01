8cdf6e0cd022db4561829455c8e131079ae61d5f
SeemdmAx <seemdmax@gmail.com>
2021-05-01 19:51:45 +0200

    booting

--- ExodusCore/OpenExodusBoot.lua
+++ ExodusCore/OpenExodusBoot.lua
@@ -128,7 +128,7 @@ function OpenExodusBoot.bootUp()
     shell.execute("reboot")
   end
   OpenExodusGui.drawBorder(2, 1, 158, 48, 0x008CCD)
-  OpenExodusGui.drawOpenExodusLogo(OpenExodusGui.getXY(middle, topMiddle, "██  ████__████ █◤██ ██ ██████◢█◤◥█████◤◥█████◥███◤████◤", 0), 0xFFFFFF, 0x008CCD, 0xFFE400)
+  OpenExodusGui.drawOpenExodusLogo(OpenExodusGui.getXY("middle", "topMiddle", "██  ████__████ █◤██ ██ ██████◢█◤◥█████◤◥█████◥███◤████◤", 0), 0xFFFFFF, 0x008CCD, 0xFFE400)
 
   io.read()
 end
