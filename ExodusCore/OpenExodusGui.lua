9e9971475edfda5d32e968d07b620c264ec24737
SeemdmAx <seemdmax@gmail.com>
2021-05-03 08:02:34 +0200

    booting

--- ExodusCore/OpenExodusGui.lua
+++ ExodusCore/OpenExodusGui.lua
@@ -90,7 +90,8 @@ function OpenExodusGui.getXY(horizontal, vertical, text, textHeight)
   if horizontal == "left" then
     horizontalReturn = 4
   elseif horizontal == "middle" then
-    horizontalReturn = math.floor(tonumber(OpenExodusGui.getResolution("width")) / 2 - tonumber(string.len(text)) / 2)
+    horizontalReturn = math.floor((tonumber(OpenExodusGui.getResolution("width")) / 2) - (tonumber(string.len(text)) / 2))
+    print(tonumber(string.len(text)) / 2)
   elseif horizontal == "right" then
     horizontalReturn = math.floor(tonumber(OpenExodusGui.getResolution("width")) - 4 - tonumber(string.len(text)))
   end
